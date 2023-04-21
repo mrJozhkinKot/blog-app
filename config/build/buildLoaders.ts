d1bb7a386a42420a9c56ebc6d661b6e9bf5878f0
mrJozhkinKot <julia.v.pike@gmail.com>
2023-04-21 10:22:36 +0300

    feat: set css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,35 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
+import { BuildOptions } from './types/config';
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          isDev? 'style-loader': MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+                modules: {
+                  auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                  localIdentName: isDev
+                  ? '[path][name]__[local]--[hash:base64:8]'
+                  : '[hash:base64:8]'
+                },
+            }
+          },
+          "sass-loader",
+        ],
+    }
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+       }
+
     return [
-        {
-          test: /\tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-         }
+        typescriptLoader,
+        cssLoader
      ]
 }
