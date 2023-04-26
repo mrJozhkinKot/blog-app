73e1b755c28b1a206f3dc62ec391b50624d32699
mrJozhkinKot <julia.v.pike@gmail.com>
2023-04-26 16:52:24 +0300

    feat: add theme context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React, { Suspense } from "react";
-import { Counter } from "./components/Counter";
-import './index.scss'; 
+import { Suspense, useContext, useState } from "react";
+import './styles/index.scss';
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { useTheme } from "./theme/useTheme";
+
+export enum Theme {
+    LIGHT = 'light',
+    DARK = 'dark',
+}
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
                 <Suspense fallback={<div>Loading...</div>}>
