14dded6ed7d5ede845ebadd469ada2dce49f0bb1
mrJozhkinKot <julia.v.pike@gmail.com>
2023-05-10 18:00:43 +0300

    feat: add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,6 +4,8 @@ import { AppRouter } from "./providers/router";
 import { Navbar } from "widgets/Navbar";
 import { classNames } from 'shared/lib/classNames';
 import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export enum Theme {
     LIGHT = 'light',
@@ -14,11 +16,13 @@ const App = () => {
     const {theme} = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Suspense fallback=''>
             <Navbar />
             <div className='content-page'>
                 <Sidebar />
                 <AppRouter />
             </div>
+            </Suspense>
         </div>
     )
 }
